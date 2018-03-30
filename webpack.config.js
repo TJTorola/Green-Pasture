588dfc583a4ee79406f8d445cf736b6e948e05de
Tyler Torola <tyler@tjt.codes>
2018-03-29 21:10:37 -0700

    Add webpack-copy

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,51 @@
-const webpack = require('webpack');
-const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
 const ROOT = path.resolve(__dirname);
-const DST_DIR = path.join(ROOT, 'dst');
-const SRC_DIR = path.join(ROOT, 'src');
-const JS_DIR = path.join(SRC_DIR, 'js');
-const STATIC_DIR = path.join(SRC_DIR, 'static');
+const DST_DIR = path.join(ROOT, "dst");
+const SRC_DIR = path.join(ROOT, "src");
+const JS_DIR = path.join(SRC_DIR, "js");
+const STATIC_DIR = path.join(SRC_DIR, "static");
 
 const config = {
   entry: {
-    Pasture: './src/js/index.jsx',
+    Pasture: "./src/js/index.jsx"
   },
   output: {
     path: DST_DIR,
-    filename: '[name].js',
-    libraryTarget: 'umd',
-    library: '[name]'
+    filename: "[name].js",
+    libraryTarget: "umd",
+    library: "[name]"
   },
   resolve: {
     alias: {
-      '~': JS_DIR,
+      "~": JS_DIR
     },
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"]
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: [/node_modules/],
-        use: ['babel-loader'],
-      },
-    ],
+        use: ["babel-loader"]
+      }
+    ]
   },
+  plugins: [new CopyWebpackPlugin([{ from: STATIC_DIR }])],
   devServer: {
     contentBase: STATIC_DIR,
-    historyApiFallback: { index: 'index.html' },
+    historyApiFallback: { index: "index.html" },
     disableHostCheck: true,
     overlay: true,
     stats: {
       chunks: false,
-      colors: true,
+      colors: true
     },
     port: process.env.WEB_PORT || 3000
   },
-  devtool: 'source-map',
+  devtool: "source-map"
 };
 
 module.exports = config;
